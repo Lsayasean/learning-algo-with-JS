2473affbbbf15dbbc0a20dbb63818c6ce0ca5cd1
unknown <lsayasean@gmail.com>
2020-05-21 21:02:06 -0400

    invert binary tree

---
+++ invert_binary_tree.js
@@ -0,0 +1,30 @@
+// invert a binary tree
+
+function invertBinaryTree(tree) {
+	let queue = [tree]
+	while(queue.length){
+        // get first item in queue array
+		let current = queue.shift()
+		if(current == null){
+			continue
+        }
+        // holds a temp value of left before be update
+		let temp = current.left
+		current.left = current.right
+        current.right = temp
+        // once we perform the swaps we push the left and right branch into queue
+		queue.push(current.right)
+		queue.push(current.left)
+	}
+}
+
+// recursive
+
+function invertBinaryTree(tree) {
+	if(tree === null) return;
+	let temp = tree.left
+	tree.left = tree.right
+	tree.right = temp
+	invertBinaryTree(tree.left)
+	invertBinaryTree(tree.right)
+}
