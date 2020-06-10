37197cd133e20e6bb75942a919daf5f362759b19
unknown <lsayasean@gmail.com>
2020-06-10 07:21:36 -0400

    shuffled array

--- find_longest_palindrome.js
+++ find_longest_palindrome.js
@@ -3,7 +3,9 @@
 function longestPalindromicSubstring(string) {
 	let longest = ""
 	for(let i = 0; i < string.length; i++){
+		// the inner for loop goes through and create sub strings of the original string
 		for(let j = 0; j < string.length; j ++){
+			// creates a copy of the substring starting at the i index and ending in the j + 1, which would be the next item next to i
 			let subString = string.slice(i, j+1);
 			if(subString.length > longest.length && isPalindrome(subString)){
 				longest = subString
